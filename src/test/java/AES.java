d0b702fbf2ffc8e4fa38972b84005f5731b3f47d
Nagaev Mark <mark_nagaev@epam.com>
2020-08-26 15:11:57 +0300

    Next try

--- src/test/java/AES.java
+++ src/test/java/AES.java
@@ -6,29 +6,57 @@ import java.util.*;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.apache.commons.codec.binary.Base64;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import us.codecraft.xsoup.Xsoup;
 
 public class AES {
 
     private long shiftL = 0;
-    private long shiftH = 1000000;
+    private long shiftH = 100000;
 
 
-    @DataProvider(parallel = true)
+    @DataProvider()
     public Iterator<Object> getRockIterator() {
         return getRock().iterator();
     }
 
     @Test(dataProvider = "getRockIterator")
-    public void getBruteAes(String password) {
-        String result = encrypt("mark", password);
+    public void getBruteAes(String password) throws InterruptedException {
+        String result = encryptBySite("mark", password.trim());
         if (result.equals("q/A1g9srH8tDHHVz8HNWVQ==")) {//"q/A1g9srH8tDHHVz8HNWVQ=="
-            savePassword(result);
+            savePassword(password);
             System.exit(1);
         }
         //(result).isEqualTo("q/A1g9srH8tDHHVz8HNWVQ==");
+    }
+
+    public String encryptBySite(String strToEncrypt, String key)
+    {
+        RestAssured.baseURI ="https://encode-decode.com/aes-128-ecb-encrypt-online";
+        RequestSpecification request = RestAssured.given().with()
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .contentType(ContentType.URLENC).body("encryption%5Balgorithm%5D=aes-128-ecb&encryption%5BsourceText%5D="
+                        +strToEncrypt
+                        +"&encryption%5BdestinationText%5D=&encryption%5Bsecret%5D="
+                        +key
+                        +"&encryption%5Bencrypt%5D=&encryption%5B_token%5D=G6qpeCTrVM5RqNfs-ihvhpMqTE1c4hHn7VJMzSLF6ZY")
+                .when().log().all()
+                .then().log().all().request();
+
+        Response response = request.post("/");
+        response = request.post("/");
+        Document document = Jsoup.parse(response.body().asString());
+        String encoded = Xsoup.compile("//*[@id=\"encryption_destinationText\"]").evaluate(document).get();
+        encoded = encoded.substring(132,156);
+        return encoded.trim();
     }
 
     public void savePassword(String pass) {
